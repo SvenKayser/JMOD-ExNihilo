9fb84ee2031d12c81c718fca5d91f4e36c7890ff
SvenKayser <sk@urbanhosting.de>
2017-03-07 06:08:39 +0100

    indev 1.3.1-3

--- src/main/java/com/jeffpeng/jmod/exnihilo/Scripting.java
+++ src/main/java/com/jeffpeng/jmod/exnihilo/Scripting.java
@@ -3,7 +3,6 @@ package com.jeffpeng.jmod.exnihilo;
 import com.jeffpeng.jmod.JMODRepresentation;
 import com.jeffpeng.jmod.exnihilo.actions.AddSifting;
 import com.jeffpeng.jmod.primitives.ModScriptObject;
-import com.jeffpeng.jmod.primitives.OwnedObject;
 
 public class Scripting extends ModScriptObject {
 	public Scripting(JMODRepresentation owner){
